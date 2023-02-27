77a0db4a1165f19a843110fa741e3c8311761e20
uniboi <plurals@autism.exposed>
2023-02-27 13:00:29 +0100

    fix if representation

--- src/if_rep.rs
+++ src/if_rep.rs
@@ -1,7 +1,7 @@
-use sqparse::ast::IfStatement;
+use sqparse::ast::{BlockStatement, IfStatement, Statement, StatementType};
 
 use crate::{
-    get_statement_rep, parens_rep::get_parens_rep, utils::get_lead,
+    block_rep::get_block_rep, get_statement_rep, parens_rep::get_parens_rep, utils::get_lead,
 };
 
 pub fn get_if_rep(stm: &IfStatement, depth: usize) -> String {
@@ -9,12 +9,25 @@ pub fn get_if_rep(stm: &IfStatement, depth: usize) -> String {
         "if{}{}",
         get_parens_rep(&*stm.condition, depth),
         match &stm.ty {
-            sqparse::ast::IfStatementType::NoElse { body } => get_statement_rep(&*body, depth),
+            sqparse::ast::IfStatementType::NoElse { body } => get_if_body_rep(&*body, depth),
             sqparse::ast::IfStatementType::Else {
                 body,
                 else_: _,
                 else_body,
-            } => format!("{}\n{}else {}", get_statement_rep(&body.ty, depth), get_lead(depth), get_statement_rep(&*else_body, depth)),
+            } => format!(
+                "{}\n{}else{}",
+                get_if_body_rep(&body.ty, depth),
+                get_lead(depth),
+                get_if_body_rep(&*else_body, depth)
+            ),
         }
     )
+}
+
+fn get_if_body_rep(stm: &StatementType, depth: usize) -> String {
+    match &stm {
+		StatementType::If(p) => format!(" {}", get_if_rep(p, depth)),
+        StatementType::Block(p) => get_block_rep(p, depth),
+        p => format!("\n{}{}", get_lead(depth + 1), get_statement_rep(p, depth + 1)),
+    }
 }
