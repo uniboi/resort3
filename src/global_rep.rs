d28566c50eac06d5bdf7cdf3a3dde6d584ae5f83
uniboi <plurals@autism.exposed>
2023-02-27 16:22:38 +0100

    add more global def reps

--- src/global_rep.rs
+++ src/global_rep.rs
@@ -1,6 +1,10 @@
 use sqparse::ast::GlobalStatement;
 
-use crate::var_rep::{get_var_initializer_rep, get_const_rep};
+use crate::{
+    enum_rep::get_enum_rep,
+    type_rep::get_typedef_rep,
+    var_rep::{get_const_rep, get_var_definition_list_rep, get_var_initializer_rep},
+};
 
 pub fn get_global_rep(statement: &GlobalStatement, depth: usize) -> String {
     let global_rep = match &statement.definition {
@@ -9,15 +13,23 @@ pub fn get_global_rep(statement: &GlobalStatement, depth: usize) -> String {
         }
         sqparse::ast::GlobalDefinition::UntypedVar { name, initializer } => {
             const EXPLICIT_TYPES: bool = false; // TODO: read from config
-            let ty = if EXPLICIT_TYPES { "TODO_EXPLICIT_TYPES " } else { "" };
-            format!("{ty}{}{}", name.value, get_var_initializer_rep(initializer, depth))
+            let ty = if EXPLICIT_TYPES {
+                "TODO_EXPLICIT_TYPES "
+            } else {
+                ""
+            };
+            format!(
+                "{ty}{}{}",
+                name.value,
+                get_var_initializer_rep(initializer, depth)
+            )
         }
-        sqparse::ast::GlobalDefinition::TypedVar(_) => todo!(),
+        sqparse::ast::GlobalDefinition::TypedVar(p) => get_var_definition_list_rep(p, depth),
         sqparse::ast::GlobalDefinition::Const(p) => get_const_rep(p, depth),
-        sqparse::ast::GlobalDefinition::Enum(_) => todo!(),
+        sqparse::ast::GlobalDefinition::Enum(p) => get_enum_rep(p, depth),
         sqparse::ast::GlobalDefinition::Class(_) => todo!(),
         sqparse::ast::GlobalDefinition::Struct(_) => todo!(),
-        sqparse::ast::GlobalDefinition::Type(_) => todo!(),
+        sqparse::ast::GlobalDefinition::Type(p) => get_typedef_rep(p, depth),
     };
     format!("global {global_rep}")
 }
