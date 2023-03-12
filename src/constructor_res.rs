b3cf882274ab411e662c3c2603c5544993e19080
uniboi <plurals@autism.exposed>
2023-03-12 21:19:17 +0100

    fix constructor rep

--- src/constructor_res.rs
+++ src/constructor_res.rs
@@ -1,7 +1,22 @@
 use sqparse::ast::ConstructorDefinitionStatement;
 
-use crate::function_rep::get_function_def_rep;
+use crate::{function_rep::get_function_def_rep, tokens::get_token};
 
 pub fn get_constructor_def_rep(c: &ConstructorDefinitionStatement, depth: usize) -> String {
-    format!("function {}::constructor{}", c.last_name.value, get_function_def_rep(&c.definition, depth))
+    format!(
+        "{} {}{}{}{}{}",
+        get_token(c.function, "function"),
+        c.namespaces
+            .iter()
+            .map(|(namespace, seperator)| format!(
+                "{}{}",
+                namespace.value,
+                get_token(seperator, "::")
+            ))
+            .collect::<String>(),
+        c.last_name.value,
+        get_token(c.last_namespace, "::"),
+		get_token(c.constructor, "constructor"),
+        get_function_def_rep(&c.definition, depth)
+    )
 }
