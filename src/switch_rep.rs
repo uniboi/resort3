f351dd65fd236eeeb29f5f5e5236e76f249fbde9
uniboi <plurals@autism.exposed>
2023-03-12 22:49:25 +0100

    switch token comments

--- src/switch_rep.rs
+++ src/switch_rep.rs
@@ -1,19 +1,22 @@
-use sqparse::ast::{SwitchCase, SwitchStatement};
+use sqparse::ast::{Statement, SwitchCase, SwitchStatement};
 
 use crate::{get_expression_rep, get_statement_rep, tokens::get_token, utils::get_lead};
 
 pub fn get_switch_rep(stm: &SwitchStatement, depth: usize) -> String {
     let lead = get_lead(depth);
     format!(
-        "switch{} {} {}\n{lead}{{\n{}\n{lead}}}",
+        "{}{} {} {}\n{lead}{}\n{}\n{lead}{}",
+        get_token(stm.switch, "switch"),
         get_token(stm.open_condition, "("),
         get_expression_rep(&*stm.condition, depth),
         get_token(stm.close_condition, ")"),
+        get_token(stm.open_cases, "{"),
         stm.cases
             .iter()
             .map(|case| get_case_rep(case, depth + 1))
             .collect::<Vec<_>>()
-            .join("\n")
+            .join("\n"),
+        get_token(stm.close_cases, "}"),
     )
 }
 
@@ -21,24 +24,35 @@ fn get_case_rep(case: &SwitchCase, depth: usize) -> String {
     let case_lead = get_lead(depth);
     let body_lead = get_lead(depth + 1);
     match &case.condition {
-        sqparse::ast::SwitchCaseCondition::Default { default: _ } => format!(
-            "{case_lead}default:\n{body_lead}{}",
-            case.body
-                .iter()
-                .map(|body| get_statement_rep(&body.ty, depth + 1))
-                .collect::<Vec<_>>()
-                .join("\n")
+        sqparse::ast::SwitchCaseCondition::Default { default } => format!(
+            "{case_lead}{}{}{}",
+            get_token(default, "default"),
+            get_token(case.colon, ":"),
+            if case.body.len() > 0 {
+                format!("\n{body_lead}{}", get_case_body_rep(&case.body, depth))
+            } else {
+                String::new()
+            }
         ),
-        sqparse::ast::SwitchCaseCondition::Case { case: _, value } => {
+        sqparse::ast::SwitchCaseCondition::Case { case: c, value } => {
             format!(
-                "{case_lead}case {}:\n{body_lead}{}",
+                "{case_lead}{} {}{}{}",
+                get_token(c, "case"),
                 get_expression_rep(&*value, depth + 1),
-                case.body
-                    .iter()
-                    .map(|body| get_statement_rep(&body.ty, depth + 1))
-                    .collect::<Vec<_>>()
-                    .join("\n")
+                get_token(case.colon, ":"),
+                if case.body.len() > 0 {
+                    format!("\n{body_lead}{}", get_case_body_rep(&case.body, depth))
+                } else {
+                    String::new()
+                }
             )
         }
     }
+}
+
+fn get_case_body_rep(body: &Vec<Statement>, depth: usize) -> String {
+    body.iter()
+        .map(|body| get_statement_rep(&body.ty, depth + 1))
+        .collect::<Vec<_>>()
+        .join("\n")
 }
