aa798a93377f31779fb4f9b5509714e10cd584fc
uniboi <plurals@autism.exposed>
2023-03-12 21:28:17 +0100

    enum token comments

--- src/enum_rep.rs
+++ src/enum_rep.rs
@@ -1,25 +1,28 @@
 use sqparse::ast::{EnumDefinitionStatement, EnumEntry};
 
-use crate::{utils::get_lead, var_rep::get_var_initializer_rep};
+use crate::{tokens::get_token, utils::get_lead, var_rep::get_var_initializer_rep};
 
 pub fn get_enum_rep(p: &EnumDefinitionStatement, depth: usize) -> String {
     let lead = get_lead(depth);
     format!(
-        "enum {}\n{}{{\n{}\n{}}}",
+        "{} {}\n{}{}\n{}\n{}{}",
+        get_token(p.enum_, "enum"),
         p.name.value,
         lead,
+        get_token(p.open, "{"),
         p.entries
             .iter()
             .map(|entry| get_enum_entry_rep(entry, depth + 1))
             .collect::<Vec<_>>()
             .join(",\n"),
-        lead
+        lead,
+        get_token(p.close, "}"),
     )
 }
 
 fn get_enum_entry_rep(entry: &EnumEntry, depth: usize) -> String {
     let lead = get_lead(depth);
-	format!(
+    format!(
         "{lead}{}{}",
         entry.name.value,
         match &entry.initializer {
