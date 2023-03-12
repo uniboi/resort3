60466b9ad6230ec47f84b8725f640cff055c3a1c
uniboi <plurals@autism.exposed>
2023-03-12 23:12:52 +0100

    try token comments

--- src/try_rep.rs
+++ src/try_rep.rs
@@ -1,17 +1,27 @@
 use sqparse::ast::{ThrowStatement, TryCatchStatement};
 
-use crate::{block_rep::get_inset_statement_rep, get_expression_rep, utils::get_lead};
+use crate::{
+    block_rep::get_inset_statement_rep, get_expression_rep, tokens::get_token, utils::get_lead,
+};
 
 pub fn throw_rep(p: &ThrowStatement, depth: usize) -> String {
-    format!("throw {}", get_expression_rep(&*p.value, depth))
+    format!(
+        "{} {}",
+        get_token(p.throw, "throw"),
+        get_expression_rep(&*p.value, depth)
+    )
 }
 
 pub fn get_try_rep(p: &TryCatchStatement, depth: usize) -> String {
     format!(
-        "try{}\n{}catch( {} ){}",
+        "{}{}\n{}{}{} {} {}{}",
+        get_token(p.try_, "try"),
         get_inset_statement_rep(&p.body.ty, depth),
         get_lead(depth),
+        get_token(p.catch, "catch"),
+        get_token(p.open, "("),
         p.catch_name.value,
+        get_token(p.close, ")"),
         get_inset_statement_rep(&p.catch_body, depth)
     )
 }
