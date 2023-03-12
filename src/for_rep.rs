f432c84952d7013c0dfd72ae7c6e452081c1abb0
uniboi <plurals@autism.exposed>
2023-03-12 21:36:06 +0100

    for statement token comments

--- src/for_rep.rs
+++ src/for_rep.rs
@@ -1,10 +1,14 @@
 use sqparse::ast::ForStatement;
 
-use crate::{get_expression_rep, var_rep::get_var_definition_list_rep, get_statement_rep};
+use crate::{
+    get_expression_rep, get_statement_rep, tokens::get_token, var_rep::get_var_definition_list_rep,
+};
 
 pub fn get_for_rep(stm: &ForStatement, depth: usize) -> String {
     format!(
-        "for( {}; {}; {} ){}",
+        "{}{} {}{} {}{} {} {}{}",
+        get_token(stm.for_, "for"),
+        get_token(stm.open, "("),
         match &stm.initializer {
             Some(initializer) => match initializer {
                 sqparse::ast::ForDefinition::Expression(initializer) =>
@@ -14,14 +18,17 @@ pub fn get_for_rep(stm: &ForStatement, depth: usize) -> String {
             },
             None => String::new(),
         },
+        get_token(stm.semicolon_1, ";"),
         match &stm.condition {
             Some(condition) => get_expression_rep(condition, depth),
             None => String::new(),
         },
+        get_token(stm.semicolon_2, ";"),
         match &stm.increment {
             Some(increment) => get_expression_rep(increment, depth),
             None => String::new(),
         },
-		get_statement_rep(&*stm.body, depth)
+        get_token(stm.close, ")"),
+        get_statement_rep(&*stm.body, depth)
     )
 }
