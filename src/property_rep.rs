cf0b3f8e9edc9d15ebb540d0e011739f1b4e52de
uniboi <plurals@autism.exposed>
2023-03-12 22:28:06 +0100

    property token comments

--- src/property_rep.rs
+++ src/property_rep.rs
@@ -1,16 +1,17 @@
-use crate::get_expression_rep;
+use crate::{get_expression_rep, tokens::get_token};
 
 pub fn get_property_rep(exp: &sqparse::ast::PropertyExpression, depth: usize) -> String {
     format!(
-        "{}.{}",
+        "{}{}{}",
         get_expression_rep(&*exp.base, depth),
+        get_token(exp.dot, "."),
         get_method_identifier_rep(&exp.property)
     )
 }
 
 fn get_method_identifier_rep(exp: &sqparse::ast::MethodIdentifier) -> String {
-    String::from(match exp {
-        sqparse::ast::MethodIdentifier::Identifier(exp) => exp.value,
-        sqparse::ast::MethodIdentifier::Constructor(_) => "constructor",
-    })
+    match exp {
+        sqparse::ast::MethodIdentifier::Identifier(exp) => String::from(exp.value),
+        sqparse::ast::MethodIdentifier::Constructor(token) => get_token(token, "constructor"),
+    }
 }
