47cf7f3683c66124b83bd875c8a86e1b1ec74d9a
uniboi <plurals@autism.exposed>
2023-03-12 21:31:43 +0100

    prefix / postfix token comments

--- src/fix_rep.rs
+++ src/fix_rep.rs
@@ -1,32 +1,40 @@
-use sqparse::ast::{PrefixExpression, PostfixExpression};
+use sqparse::ast::{PostfixExpression, PrefixExpression};
 
-use crate::get_expression_rep;
+use crate::{get_expression_rep, tokens::get_token};
 
 pub fn get_prefixed_expression_rep(exp: &PrefixExpression, depth: usize) -> String {
-	format!("{}{}", get_prefix_rep(&exp.operator), get_expression_rep(&*exp.value, depth))
+    format!(
+        "{}{}",
+        get_prefix_rep(&exp.operator),
+        get_expression_rep(&*exp.value, depth)
+    )
 }
 
 fn get_prefix_rep(op: &sqparse::ast::PrefixOperator) -> String {
-    String::from(match op {
-        sqparse::ast::PrefixOperator::Negate(_) => "-",
-        sqparse::ast::PrefixOperator::LogicalNot(_) => "!",
-        sqparse::ast::PrefixOperator::BitwiseNot(_) => "~",
-        sqparse::ast::PrefixOperator::Increment(_) => "++",
-        sqparse::ast::PrefixOperator::Decrement(_) => "--",
-		// These are keywords that require a space after
-        sqparse::ast::PrefixOperator::Typeof(_) => "typeof ",
-        sqparse::ast::PrefixOperator::Clone(_) => "clone ",
-        sqparse::ast::PrefixOperator::Delete(_) => "delete ",
-    })
+    match op {
+        sqparse::ast::PrefixOperator::Negate(token) => get_token(token, "-"),
+        sqparse::ast::PrefixOperator::LogicalNot(token) => get_token(token, "!"),
+        sqparse::ast::PrefixOperator::BitwiseNot(token) => get_token(token, "~"),
+        sqparse::ast::PrefixOperator::Increment(token) => get_token(token, "++"),
+        sqparse::ast::PrefixOperator::Decrement(token) => get_token(token, "--"),
+        // These are keywords that require a space after
+        sqparse::ast::PrefixOperator::Typeof(token) => format!("{} ", get_token(token, "typeof")),
+        sqparse::ast::PrefixOperator::Clone(token) => format!("{} ", get_token(token, "clone")),
+        sqparse::ast::PrefixOperator::Delete(token) => format!("{} ", get_token(token, "delete")),
+    }
 }
 
 pub fn get_postfixed_expression_rep(exp: &PostfixExpression, depth: usize) -> String {
-	format!("{}{}", get_expression_rep(&*exp.value, depth), get_postfix_rep(&exp.operator))
+    format!(
+        "{}{}",
+        get_expression_rep(&*exp.value, depth),
+        get_postfix_rep(&exp.operator)
+    )
 }
 
 fn get_postfix_rep(op: &sqparse::ast::PostfixOperator) -> String {
-    String::from(match op {
-        sqparse::ast::PostfixOperator::Increment(_) => "++",
-        sqparse::ast::PostfixOperator::Decrement(_) => "--",
-    })
+    match op {
+        sqparse::ast::PostfixOperator::Increment(token) => get_token(token, "++"),
+        sqparse::ast::PostfixOperator::Decrement(token) => get_token(token, "--"),
+    }
 }
