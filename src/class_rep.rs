38b599e12adb1364135ea5703abdba910abd0462
uniboi <plurals@autism.exposed>
2023-03-12 20:52:46 +0100

    class comments

--- src/class_rep.rs
+++ src/class_rep.rs
@@ -2,15 +2,20 @@ use sqparse::ast::{
     ClassDefinition, ClassDefinitionStatement, ClassExpression, ClassExtends, ClassMember,
 };
 
-use crate::{get_expression_rep, table_rep::get_slot_rep, utils::get_lead};
+use crate::{get_expression_rep, table_rep::get_slot_rep, tokens::get_token, utils::get_lead};
 
 pub fn get_class_expression_rep(p: &ClassExpression, depth: usize) -> String {
-    format!("class {}", get_class_def_rep(&p.definition, depth))
+    format!(
+        "{} {}",
+        get_token(p.class, "class"),
+        get_class_def_rep(&p.definition, depth)
+    )
 }
 
 pub fn get_class_statement_rep(p: &ClassDefinitionStatement, depth: usize) -> String {
     format!(
-        "class {} {}",
+        "{} {} {}",
+        get_token(p.class, "class"),
         get_expression_rep(&*p.name, depth),
         get_class_def_rep(&p.definition, depth)
     )
@@ -19,12 +24,14 @@ pub fn get_class_statement_rep(p: &ClassDefinitionStatement, depth: usize) -> St
 fn get_class_def_rep(def: &ClassDefinition, depth: usize) -> String {
     let lead = get_lead(depth);
     format!(
-        "{}\n{lead}{{{}{lead}}}",
+        "{}\n{lead}{}{}{lead}{}",
         match &def.extends {
             Some(ext) => get_class_extend_rep(ext, depth),
             None => String::new(),
         },
-        get_class_members_rep(&def.members, depth + 1)
+        get_token(def.open, "}"),
+        get_class_members_rep(&def.members, depth + 1),
+        get_token(def.close, "}")
     )
 }
 
@@ -38,8 +45,8 @@ fn get_class_members_rep(members: &Vec<ClassMember>, depth: usize) -> String {
             .map(|member| format!(
                 "{lead}{}{}\n",
                 match &member.static_ {
-                    Some(_) => "static ",
-                    None => "",
+                    Some(token) => format!("{} ", get_token(token, "static")),
+                    None => String::new(),
                 },
                 get_slot_rep(&member.slot, depth)
             ))
@@ -48,5 +55,9 @@ fn get_class_members_rep(members: &Vec<ClassMember>, depth: usize) -> String {
 }
 
 fn get_class_extend_rep(ext: &ClassExtends, depth: usize) -> String {
-    format!("extends {}", get_expression_rep(&*ext.name, depth))
+    format!(
+        "{} {}",
+        get_token(ext.extends, "extends"),
+        get_expression_rep(&*ext.name, depth)
+    )
 }
