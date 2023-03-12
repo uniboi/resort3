3f538ca8ba194f8ade72c9628409d27fe7bdad6b
uniboi <plurals@autism.exposed>
2023-03-12 21:38:43 +0100

    foreach token comments

--- src/foreach_rep.rs
+++ src/foreach_rep.rs
@@ -1,24 +1,20 @@
 use sqparse::ast::{ForeachIndex, ForeachStatement, Identifier, Type};
 
-use crate::{block_rep::get_inset_statement_rep, get_expression_rep, type_rep::get_typed_type_rep};
+use crate::{block_rep::get_inset_statement_rep, get_expression_rep, type_rep::get_typed_type_rep, tokens::get_token};
 
 pub fn get_foreach_rep(stm: &ForeachStatement, depth: usize) -> String {
-    // format!(
-    //     "foreach( {} ){}",
-    //     match &stm.index {
-    //         Some(idx) => format!("{}, ", get_foreach_index_rep(idx, depth)),
-    //         None => String::new(),
-    //     },
-    //     get_inset_statement_rep(&*stm.body, depth)
-    // )
     format!(
-        "foreach({}{} in {} ){}",
+        "{}{}{}{} {} {} {}{}",
+		get_token(stm.foreach, "foreach"),
+		get_token(stm.open, "("),
         match &stm.index {
             Some(idx) => format!("{},", get_foreach_index_rep(idx, depth)),
             None => String::new(),
         },
         get_foreach_value_rep(&stm.value_type, &stm.value_name, depth),
+		get_token(stm.in_, "in"),
         get_expression_rep(&*stm.array, depth),
+		get_token(stm.close, ")"),
         get_inset_statement_rep(&*stm.body, depth)
     )
 }
