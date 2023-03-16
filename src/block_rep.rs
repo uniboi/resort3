564ba0380668445fe3bd50b73555c56e1671493e
uniboi <plurals@autism.exposed>
2023-03-16 21:22:51 +0100

    fix whitespace block lines hopefully for good

--- src/block_rep.rs
+++ src/block_rep.rs
@@ -6,7 +6,7 @@ pub fn get_block_rep(block: &BlockStatement, depth: usize) -> String {
     let inline_pre = get_lead(depth);
     let statements_pre = get_lead(depth + 1);
     let pre = format!("\n{inline_pre}");
-	// TODO: still produces lines containing only whitespace because of pre_lines
+    // TODO: still produces lines containing only whitespace because of pre_lines
     format!(
         "{pre}{}\n{}{pre}{}",
         get_token(block.open, "{", depth),
@@ -15,7 +15,12 @@ pub fn get_block_rep(block: &BlockStatement, depth: usize) -> String {
             .iter()
             .map(|statement| {
                 let rep = get_statement_rep(&statement.ty, depth + 1);
-                if rep.find("//") == Some(0) {
+                let lines = rep.split("\n").collect::<Vec<_>>();
+                let first_line = lines.get(0);
+				
+                if rep.find("//") == Some(0)
+                    || (matches!(first_line, Some(_)) && first_line.unwrap().trim().is_empty())
+                {
                     rep
                 } else {
                     format!("{statements_pre}{rep}")
