417d68be1a5d0a52ed41b84f3bd9c6732cce0c83
uniboi <plurals@autism.exposed>
2023-03-20 22:43:28 +0100

    default config initialization

--- src/config.rs
+++ src/config.rs
@@ -5,6 +5,39 @@ use std::io::BufReader;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
+pub struct ConfigFile {
+    // pool settings
+    pub padding: Option<bool>,
+    pub gap: Option<bool>,
+    pub inline: Option<bool>,
+
+    // pool overrides
+    pub semicolons: Option<bool>,
+    pub delaythread_padding: Option<bool>,
+    pub delaythread_gap: Option<bool>,
+    pub array_oneliner_max: Option<usize>,
+    pub array_oneliner_definition_padding: Option<bool>,
+    pub expect_padding: Option<bool>,
+    pub trim_float: Option<bool>,
+    pub vector_padding: Option<bool>,
+    pub functionref_oneliner_args_max: Option<usize>,
+    pub non_generic_type_padding: Option<bool>,
+    pub table_oneliner_max: Option<usize>,
+    pub if_gap: Option<bool>,
+    pub if_padding: Option<bool>,
+    pub if_inline: Option<bool>,
+    pub for_gap: Option<bool>,
+    pub for_padding: Option<bool>,
+    pub for_inline: Option<bool>,
+    pub foreach_gap: Option<bool>,
+    pub foreach_padding: Option<bool>,
+    pub foreach_inline: Option<bool>,
+    pub while_gap: Option<bool>,
+    pub while_padding: Option<bool>,
+    pub while_inline: Option<bool>,
+    pub do_while_inline: Option<bool>,
+}
+
 pub struct Config {
     /// Add semicolons to statements
     pub semicolons: bool,
@@ -38,8 +71,8 @@ pub struct Config {
     pub for_gap: bool,
     /// add padding in the condition
     pub for_padding: bool,
-	/// single inline statement
-	pub for_inline: bool,
+    /// single inline statement
+    pub for_inline: bool,
     /// add a gap between `for` and the condition
     pub foreach_gap: bool,
     /// add padding in the condition
@@ -50,18 +83,107 @@ pub struct Config {
     pub while_gap: bool,
     /// add padding in the condition
     pub while_padding: bool,
-	/// inline single statement
-	pub while_inline: bool,
-	pub do_while_inline: bool,
+    /// inline single statement
+    pub while_inline: bool,
+    /// inline single statement
+    pub do_while_inline: bool,
 }
 
 impl Config {
+    pub fn new() -> Self {
+        Self {
+            semicolons: false,
+            delaythread_padding: true,
+            delaythread_gap: false,
+            array_oneliner_max: 5,
+            array_oneliner_definition_padding: true,
+            expect_padding: true,
+            trim_float: false,
+            vector_padding: true,
+            functionref_oneliner_args_max: 5,
+            non_generic_type_padding: false,
+            table_oneliner_max: 3,
+            if_gap: true,
+            if_padding: true,
+            if_inline: false,
+            for_gap: true,
+            for_padding: true,
+            for_inline: false,
+            foreach_gap: true,
+            foreach_padding: true,
+            foreach_inline: false,
+            while_gap: true,
+            while_padding: true,
+            while_inline: false,
+            do_while_inline: false,
+        }
+    }
+
     pub fn from_path(path: &String) -> Result<Self, Box<dyn Error>> {
         let file = File::open(path)?;
         let reader = BufReader::new(file);
 
-        let p: Self = serde_json::from_reader(reader)?;
+        let default: Config = Config::new();
+        let p: ConfigFile = serde_json::from_reader(reader)?;
 
-        Ok(p)
+        Ok(Self {
+            semicolons: p.semicolons.unwrap_or(default.semicolons),
+            delaythread_padding: p
+                .delaythread_padding
+                .unwrap_or(p.padding.unwrap_or(default.delaythread_padding)),
+            delaythread_gap: p
+                .delaythread_gap
+                .unwrap_or(p.gap.unwrap_or(default.delaythread_gap)),
+            array_oneliner_max: p.array_oneliner_max.unwrap_or(default.array_oneliner_max),
+            array_oneliner_definition_padding: p.array_oneliner_definition_padding.unwrap_or(
+                p.padding
+                    .unwrap_or(default.array_oneliner_definition_padding),
+            ),
+            expect_padding: p
+                .expect_padding
+                .unwrap_or(p.padding.unwrap_or(default.expect_padding)),
+            trim_float: p.trim_float.unwrap_or(default.trim_float),
+            vector_padding: p
+                .vector_padding
+                .unwrap_or(p.padding.unwrap_or(default.vector_padding)),
+            functionref_oneliner_args_max: p
+                .functionref_oneliner_args_max
+                .unwrap_or(default.functionref_oneliner_args_max),
+            non_generic_type_padding: p
+                .non_generic_type_padding
+                .unwrap_or(p.padding.unwrap_or(default.non_generic_type_padding)),
+            table_oneliner_max: p.table_oneliner_max.unwrap_or(default.table_oneliner_max),
+            if_gap: p.if_gap.unwrap_or(p.gap.unwrap_or(default.if_gap)),
+            if_padding: p
+                .if_padding
+                .unwrap_or(p.padding.unwrap_or(default.if_padding)),
+            if_inline: p.if_inline.unwrap_or(p.inline.unwrap_or(default.if_inline)),
+            for_gap: p.for_gap.unwrap_or(p.gap.unwrap_or(default.for_gap)),
+            for_padding: p
+                .for_padding
+                .unwrap_or(p.padding.unwrap_or(default.for_padding)),
+            for_inline: p
+                .for_inline
+                .unwrap_or(p.inline.unwrap_or(default.for_inline)),
+            foreach_gap: p
+                .foreach_gap
+                .unwrap_or(p.gap.unwrap_or(default.foreach_gap)),
+            foreach_padding: p
+                .foreach_padding
+                .unwrap_or(p.padding.unwrap_or(default.foreach_padding)),
+            foreach_inline: p
+                .foreach_inline
+                .unwrap_or(p.inline.unwrap_or(default.foreach_inline)),
+            while_gap: p.while_gap.unwrap_or(p.gap.unwrap_or(default.while_gap)),
+            while_padding: p
+                .while_padding
+                .unwrap_or(p.padding.unwrap_or(default.while_padding)),
+            while_inline: p
+                .while_inline
+                .unwrap_or(p.inline.unwrap_or(default.while_inline)),
+            do_while_inline: p
+                .do_while_inline
+                .unwrap_or(p.inline.unwrap_or(default.do_while_inline)),
+        })
     }
 }
